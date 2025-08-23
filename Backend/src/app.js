5e23d45ed3eb70feeb23997b7b7517e7178fa536
gurushant <mattigurushant@gmail.com>
2025-08-23 19:49:24 +0530

    front end using vite(react)

---
+++ Backend/src/app.js
@@ -0,0 +1,29 @@
+const express = require('express');
+const cookieParser = require('cookie-parser');
+const cors = require('cors');
+
+
+/* Routes */
+const authRoutes = require('./routes/auth.routes');
+const chatRoutes = require("./routes/chat.routes");
+
+
+const app = express();
+
+
+
+/* using middlewares */
+app.use(cors({
+    origin: 'http://localhost:5173',
+    credentials: true
+}))
+app.use(express.json());
+app.use(cookieParser());
+
+
+
+/* Using Routes */
+app.use('/api/auth', authRoutes);
+app.use('/api/chat', chatRoutes);
+
+module.exports = app;
